f498a6c6bd6e914df4367f8ea1e7fe124093eb8f
Niclas-Gustafsson <niclas.gustafsson@chasacademy.se>
2022-04-04 23:12:54 +0200

    trigger build

--- project/recipe-app/app/Http/Controllers/API/AuthController.php
+++ project/recipe-app/app/Http/Controllers/API/AuthController.php
@@ -12,13 +12,16 @@ use Validator;
 
 class AuthController extends BaseController
 {
-    public function index() {
+    public function index()
+    {
         $users = User::all();
+        //Add an stupid comment to trigger build process
 
         return $this->responseHandler(UserResource::collection($users), 'Users has been recieved.');
     }
 
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
 
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
@@ -27,7 +30,7 @@ class AuthController extends BaseController
             'password_confirmation' => 'required|min:8'
         ]);
 
-        if($validator->fails()) {
+        if ($validator->fails()) {
             return $this->errorHandler($validator->errors());
         }
 
@@ -40,32 +43,32 @@ class AuthController extends BaseController
         return $this->responseHandler(new UserResource($user), 'User created');
     }
 
-    public function getUser($id) {
+    public function getUser($id)
+    {
         $user = User::find($id);
 
 
-        if(is_null($user)) {
+        if (is_null($user)) {
             return $this->errorHandler('User not found');
         }
 
-        if($user->id !== Auth::user()->id) {
-            return $this->errorHandler('Unauthorized', ['error'=>'Unauthorised']);
+        if ($user->id !== Auth::user()->id) {
+            return $this->errorHandler('Unauthorized', ['error' => 'Unauthorised']);
         } else {
 
             return $this->responseHandler(new UserResource($user), 'User retrieved');
         }
-
-
     }
 
-    public function login(Request $request) {
+    public function login(Request $request)
+    {
         $request->validate([
             'email' => 'required',
             'password' => 'required'
         ]);
         $userCred = $request->only('email', 'password');
 
-        if(Auth::attempt($userCred)) {
+        if (Auth::attempt($userCred)) {
             $auth = Auth::user();
             $success['id'] = $auth->id;
             $success['token'] = $auth->createToken('auth_token')->plainTextToken;
@@ -73,13 +76,13 @@ class AuthController extends BaseController
 
             return $this->responseHandler($success, 'User logged in.');
         } else {
-            return $this->errorHandler('Unauthorised.', ['error'=>'Unauthorised']);
+            return $this->errorHandler('Unauthorised.', ['error' => 'Unauthorised']);
         }
     }
 
-    public function logout() {
+    public function logout()
+    {
         auth()->user()->tokens()->delete();
         return response()->json('Logged out successfully, tokens has been deleted.');
     }
-
 }
